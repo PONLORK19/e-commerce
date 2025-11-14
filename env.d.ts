94e4e3b85bdb16f14ce7646401de59d3e75e5734
Ponlork <ponlork2535@gmail.com>
2025-11-14 16:51:18 +0700

    TP01

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="vite/client" />
+// src/shims-vue.d.ts
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
